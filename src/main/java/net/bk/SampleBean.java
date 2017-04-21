f57bece10d4635414e1d536425ffe8ef74580f37
Brian Keating <b@briankeating.net>
2017-04-21 21:59:54 +0100

    Initial commiti

---
+++ src/main/java/net/bk/SampleBean.java
@@ -0,0 +1,16 @@
+package net.bk;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("myBean")
+public class SampleBean {
+
+    @Value("${greeting}")
+    private String say;
+
+    public String saySomething() {
+        return say;
+    }
+
+}
