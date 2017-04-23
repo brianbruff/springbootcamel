d40034751a642aae993643be63bc70f38652dfd9
Brian Keating <b@briankeating.net>
2017-04-23 11:52:21 +0100

    testing injection

--- src/main/java/net/bk/SampleBean.java
+++ src/main/java/net/bk/SampleBean.java
@@ -1,10 +1,14 @@
 package net.bk;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("myBean")
-public class SampleBean {
+public class SampleBean{
+
+    @Autowired
+    SampleChildBean child;
 
     @Value("${greeting}")
     private String say;
